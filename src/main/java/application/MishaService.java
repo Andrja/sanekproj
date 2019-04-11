bf077b76f6d7a26d853323eacc85abc111380904
unknown <mihails.molokovs@gmail.com>
2019-04-11 20:52:25 +0300

    test-conflict

--- src/main/java/application/MishaService.java
+++ src/main/java/application/MishaService.java
@@ -2,9 +2,14 @@ package application;
 
 import org.springframework.stereotype.Service;
 
+
+
 @Service
 
 public class MishaService {
+    /**
+     * return
+     */
     public Integer getMisha() {
         return 3+3;
     }
